1cd664df25f0f4ed5ef8fa81d9e295ec8c5965bc
Imagdy00 <125517448+Imagdy00@users.noreply.github.com>
2024-12-27 14:54:10 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Console.WriteLine("Enter a Number ");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine(number);
+
+            #endregion
+
+            #region Q2
+            //Console.WriteLine("Enter string to convert it into integer : ");
+            //Console.ReadLine();
+            //int Result;
+            //bool success = int.TryParse(Console.ReadLine(), out Result);
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region Q3
+            //float x = 1.5f , y = 2.5f;
+            //float Result = x + y;
+            //Console.WriteLine(Result);
+            #endregion
+
+
+            #region Q4
+            //string text = "Hello, World!";
+            //string substring = text.Substring(7, 5);
+            //Console.WriteLine(substring);
+
+            #endregion
+
+            #region Q5
+            //int x = 5;
+            //int y = x;
+            //y = 10;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            #endregion
+
+            #region Q6
+            //Person person1 = new Person();
+            //Person person2 = person1;
+            //Console.WriteLine(person1);
+            //Console.WriteLine(person2);
+
+            #endregion
+
+
+            #region Q7
+            //string str = "Mohammed";
+            //string str2 = "Magdy";
+            //string Result = str + " " + str2;
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region Q8
+            //Console.Write("Enter Principal: ");
+            //double principal = double.Parse(Console.ReadLine());
+            //Console.Write("Enter Rate: ");
+            //double rate = double.Parse(Console.ReadLine());
+            //Console.Write("Enter Time: ");
+            //double time = double.Parse(Console.ReadLine());
+
+            //double interest = (principal * rate * time) / 100;
+            //Console.WriteLine(interest);
+
+            #endregion
+
+            #region Q9
+            //Console.WriteLine("Enter Your weight in kilograms");
+            //double weight = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Your height in meters");
+            //double height = double.Parse(Console.ReadLine());
+            //double BMI = weight / (height * height ) ;
+            //Console.WriteLine(BMI);
+
+            #endregion
+
+            #region Q10
+            //Console.Write("Enter Temperature: ");
+            //int temperature = int.Parse(Console.ReadLine());
+
+            //string result = temperature < 10 ? "Just Cold" :
+            //                temperature > 30 ? "Just Hot" :
+            //                "Just Good";
+
+            //Console.WriteLine(result);
+
+            #endregion
+
+
+            #region Q11
+            //Console.Write("Enter Date (dd/MM/yyyy): ");
+            //DateTime date = DateTime.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Today’s date: {date:dd , MM , yyyy}");
+            //Console.WriteLine($"Today's date: {date:dd / MM / yyyy}");
+            //Console.WriteLine($"Today's date: {date:dd – MM – yyyy}");
+
+            #endregion
+
+            #region Q12
+            //DateTime date = new DateTime(2024, 6, 14);
+            //Console.WriteLine($"The event is on {date:MM/dd/yyyy}");
+            //// the answer : The event is on 06/14/2024
+            #endregion
+
+            #region Q13
+            int d;
+            d = Convert.ToInt32(!(30 < 20));
+            //the answer is : A value 1 will be assigned to d.
+
+            #endregion
+
+            #region Q14
+            Console.WriteLine(13 / 2 + " " + 13 % 2);
+            //the answer : 6 1
+
+            #endregion
+
+
+            #region Q15
+            //int num = 1, z = 5;
+
+
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+
+            ////the answer : 7 7
+
+
+
+            #endregion
+
+
+        }
+    }
+}
---
+++ DEMO/Program.cs
@@ -0,0 +1,163 @@
+﻿using System.Drawing;
+
+namespace DEMO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace Day1Route
+    {
+        internal class program
+        {
+            static void Main()
+            {
+                #region MyRegion
+                Console.WriteLine("mo");
+                Console.WriteLine("mo");
+                Console.WriteLine("mo");
+                Console.WriteLine("mo");
+                Console.WriteLine("mo");
+                #endregion
+
+                #region VariableDeclration
+                int Number;
+                Number = 5;
+                Console.WriteLine(Number);
+                int Number2 = 7, Number3 = 6;
+                #endregion
+
+
+                #region Data Type(Value type)
+                Int32 Number7;
+                Number7 = Number;
+                Console.WriteLine(Number7);
+                #endregion
+
+                #region Data Value Reference Type
+                Point P01; // Declare reference from type Point 
+                           //P01 can refere to object from type point 
+
+                P01 = new Point();
+                Console.WriteLine(P01.x);
+                Console.WriteLine(P01.y);
+                Console.WriteLine(P01); // this give me the namespace
+                Point P02 = new Point();
+                P02.x = 12;
+                Console.WriteLine(P01.x);
+                Console.WriteLine(P02.x);
+                P01 = P02;
+                Console.WriteLine(P01.x);
+                Console.WriteLine(P02.x);
+                #endregion
+
+
+
+                #region Object
+                P01.GetType();
+
+                P01.ToString();
+                #endregion
+
+                //Console.WriteLine("Enter Your Age : ");
+                //int age = int.Parse(Console.ReadLine());   
+                //Console.WriteLine("Enter Your GPA : ");
+                //double GPA = double.Parse(Console.ReadLine());
+                //Console.WriteLine(GPA);
+
+
+                //Console.WriteLine("Enter Your Age : ");
+                //int agee = Convert.ToInt32(Console.ReadLine());
+
+                #region Parsing
+                Console.WriteLine("Enter Your Age : ");
+
+                int age;
+                bool flag = int.TryParse(Console.ReadLine(), out int result);
+                #endregion
+
+                #region Fractions and Discards 
+                double x = 1.5;
+                float y = 1.5f;
+                decimal z = 2.5m;
+
+
+                int l = 1_000_000_000;
+
+                #endregion
+
+
+                #region Operators 
+
+                int t = 10;
+                t++;
+                ++t;
+                Console.WriteLine(t++);
+                Console.WriteLine(++t);
+                t--;
+                --t;
+                Console.WriteLine(t--);
+                Console.WriteLine(--t);
+
+
+                int sum, mul, sub, mod;
+                int number1 = 2, number2 = 6;
+                sum = number1 + number2;
+                mul = number1 * number2;
+                sub = number1 - number2;
+                mod = number1 % number2;
+
+
+
+                int w;
+                w = 4;
+
+                w += 2;
+                w -= 2;
+                w *= 2;
+                w /= 2;
+                w %= 2;
+
+                int r = 6, n = 5;
+                Console.WriteLine(r == n);
+                Console.WriteLine(r != n);
+                Console.WriteLine(r > n);
+                Console.WriteLine(r < n);
+                Console.WriteLine(r <= n);
+                Console.WriteLine(r <= n);
+
+
+                Console.WriteLine(!false);
+                Console.WriteLine(false && true);
+                Console.WriteLine(false || true);
+
+                //Bitwise operators
+                Console.WriteLine(!false);
+                Console.WriteLine(false & true);
+                Console.WriteLine(false | true);
+
+
+                //ternary operator 
+
+                bool Flag = 4 > 2;
+                Console.WriteLine(Flag);
+
+
+                String flag2 = 4 > 2 ? "true" : "false";
+                String flag3 = 4 > 2 ? "Correct" : "Not correct";
+
+                #endregion
+
+            }
+        }
+    }
+
+}
+    }
+}
